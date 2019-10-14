3efde4a3c24aa5c481eccfde77fd726965083735
pabloccna <52111134+pabloccna@users.noreply.github.com>
2019-10-14 18:36:44 -0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		int i=66;
 		System.out.println("Hola Mundo ......");
-		System.out.println("el valor de i: ",i);
+		System.out.println("el valor de i: "+i);
 	}
  
 }
