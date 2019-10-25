be434bd1127d53b530b711df1dba95d7eb0f8c34
Pablo <pablo@Pablos-MacBook-Pro.local>
2019-10-24 22:59:48 -0300

    doble linea

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,9 +3,11 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		int i=66;
 		System.out.println("----------------------------------------------------------");
+		System.out.println("----------------------------------------------------------");
 		System.out.println("Hola Mundo ......");
 		System.out.println("el valor de i: "+i);
 		System.out.println("agregue una modificacion desde vscode a git 6666");
+		System.out.println("----------------------------------------------------------");
 		System.out.println("----------------------------------------------------------");
 	}
  
