571fd59801342b5253a32867ee4e3e72e8726d1c
Pablo <pablo@Pablos-MacBook-Pro.local>
2019-10-24 23:03:23 -0300

    77777

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 		System.out.println("----------------------------------------------------------");
 		System.out.println("Hola Mundo ......");
 		System.out.println("el valor de i: "+i);
-		System.out.println("agregue una modificacion desde vscode a git 6666");
+		System.out.println("agregue una modificacion desde vscode a git 66667777");
 		System.out.println("----------------------------------------------------------");
 		System.out.println("----------------------------------------------------------");
 	}
